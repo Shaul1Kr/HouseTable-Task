61ad88f30c3b1e972e690de566daeadad8277a7f
Shaul1Kr <shaulikr07@gmail.com>
2023-08-27 20:41:58 +0300

    Fix and implement backend functions

--- server/controllers/house.js
+++ server/controllers/house.js
@@ -2,32 +2,40 @@ const House = require("../models/House");
 
 const createHouse = async (req, res) => {
   try {
-    console.log(req.body);
     const { address, curVal } = req.body;
     const newHouse = await House.create({
-      address: address,
+      address,
       currentValue: curVal,
     });
     res.status(201).json(newHouse.id);
   } catch (error) {
-    res.status(409).json({ message: err.message });
+    res.status(409).json({ message: error.message });
   }
 };
-const getAllHouses = (req, res) => {
+const getHouseDetail = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const houseDetail = await House.findByPk(id);
+    res.status(201).json(houseDetail.dataValues);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+const updateHouseRec = async (req, res) => {
   try {
     console.log(req.params, req.body);
-    res.status(201).json("Ok1");
+    const { id } = req.params;
+    const { address, curVal } = req.body;
+    const updateDetail = await House.update(
+      { address, currentValue: curVal },
+      { where: { id } }
+    );
+    const houseDetail = await House.findByPk(id);
+    res.status(201).json(houseDetail.dataValues);
   } catch (error) {
-    res.status(409).json({ message: err.message });
-  }
-};
-const updateHouseRec = (req, res) => {
-  try {
-    console.log(req.params);
-    res.status(201).json("Ok2");
-  } catch (error) {
-    res.status(409).json({ message: err.message });
+    res.status(409).json({ message: error.message });
   }
 };
 
-module.exports = { createHouse, getAllHouses, updateHouseRec };
+module.exports = { createHouse, getHouseDetail, updateHouseRec };
