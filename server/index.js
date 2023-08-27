bf6e4202bef0c1567f31f5c1edadb043a7583b41
Shaul1Kr <shaulikr07@gmail.com>
2023-08-27 12:43:07 +0300

    Init sequelized and connect to postgresql

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const { Sequelize } = require("sequelize");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json({ extended: true }));
-const PORT = process.env.PORT;
+const env = process.env;
+const PORT = env.PORT;
+const sequelize = new Sequelize(env.DATABASE, env.USER, env.PASSWORD, {
+  host: env.HOST,
+  dialect: env.DIALECT,
+});
+const connectToPostgres = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    return sequelize;
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+connectToPostgres();
+module.exports = { sequelize };
+
+//Init db table
+/*const house = require("./models/House");
+house.sync({ force: true });*/
+
 app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
