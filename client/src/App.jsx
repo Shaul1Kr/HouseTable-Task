08279f633f59f24185c92d9abc315b8ac3878831
Shaul1Kr <shaulikr07@gmail.com>
2023-08-27 18:48:27 +0300

    Main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,86 @@
+import { useState } from "react";
 import "./App.css";
+import styled from "styled-components";
 
-function App() {
-  return;
-}
+export const StyledForm = styled.form`
+  display: grid;
+  gap: 1rem;
+`;
+
+export const StyledLabel = styled.label``;
+
+export const StyledInput = styled.input`
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  &::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
+
+export const StyledButton = styled.button``;
+
+const App = () => {
+  const [address, setAddress] = useState("");
+  const [curVal, setCurVal] = useState("");
+  const [id, setId] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log({ address, curVal });
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ address, curVal }),
+    };
+    const response = await fetch(
+      "http://localhost:3000/api/houses",
+      requestOptions
+    );
+    const data = await response.json();
+    setId(data);
+    setAddress("");
+    setCurVal("");
+  };
+
+  const addressEntered = (e) => {
+    setAddress(e.target.value);
+  };
+
+  const curValEntered = (e) => {
+    setCurVal(e.target.value);
+  };
+
+  return (
+    <div>
+      <StyledForm onSubmit={handleSubmit}>
+        <StyledLabel>Address:</StyledLabel>
+        <StyledInput
+          type="text"
+          placeholder="Givatayim"
+          onChange={(e) => addressEntered(e)}
+          value={address}
+          name="address"
+          required
+        />
+        <StyledLabel>Current Value:</StyledLabel>
+        <StyledInput
+          type="number"
+          placeholder="0"
+          onChange={(e) => curValEntered(e)}
+          value={curVal}
+          name="val"
+          required
+        />
+        <StyledButton type="submit" value="Submit">
+          Submit
+        </StyledButton>
+      </StyledForm>
+      {id && <p>House Id: {id}</p>}
+    </div>
+  );
+};
 
 export default App;
