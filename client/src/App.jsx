49760006276042371a02466814a046a3e044bea2
Shaul1Kr <shaulikr07@gmail.com>
2023-08-27 20:41:06 +0300

    Added routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,84 +1,17 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HouseSubmittion from "./Pages/HouseSubmittion";
 import "./App.css";
-import styled from "styled-components";
-
-export const StyledForm = styled.form`
-  display: grid;
-  gap: 1rem;
-`;
-
-export const StyledLabel = styled.label``;
-
-export const StyledInput = styled.input`
-  &::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-  &::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-`;
-
-export const StyledButton = styled.button``;
+import HouseDetail from "./Pages/HouseDetail";
 
 const App = () => {
-  const [address, setAddress] = useState("");
-  const [curVal, setCurVal] = useState("");
-  const [id, setId] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log({ address, curVal });
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ address, curVal }),
-    };
-    const response = await fetch(
-      "http://localhost:3000/api/houses",
-      requestOptions
-    );
-    const data = await response.json();
-    setId(data);
-    setAddress("");
-    setCurVal("");
-  };
-
-  const addressEntered = (e) => {
-    setAddress(e.target.value);
-  };
-
-  const curValEntered = (e) => {
-    setCurVal(e.target.value);
-  };
-
   return (
-    <div>
-      <StyledForm onSubmit={handleSubmit}>
-        <StyledLabel>Address:</StyledLabel>
-        <StyledInput
-          type="text"
-          placeholder="Givatayim"
-          onChange={(e) => addressEntered(e)}
-          value={address}
-          name="address"
-          required
-        />
-        <StyledLabel>Current Value:</StyledLabel>
-        <StyledInput
-          type="number"
-          placeholder="0"
-          onChange={(e) => curValEntered(e)}
-          value={curVal}
-          name="val"
-          required
-        />
-        <StyledButton type="submit" value="Submit">
-          Submit
-        </StyledButton>
-      </StyledForm>
-      {id && <p>House Id: {id}</p>}
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HouseSubmittion />} />
+          <Route path="/house/:id" element={<HouseDetail />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
